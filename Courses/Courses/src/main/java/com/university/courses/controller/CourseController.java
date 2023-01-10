139779823952deec4bef22355c96a018d1fe999a
Tharshigan <41715880+Tharshu@users.noreply.github.com>
2023-01-10 21:28:08 +0530

    first commit

---
+++ Courses/Courses/src/main/java/com/university/courses/controller/CourseController.java
@@ -0,0 +1,23 @@
+package com.university.courses.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/v1/courses")
+@Slf4j
+public class CourseController {
+
+//    @GetMapping()
+//    public String getCourses(){
+////        List list = null;
+//        return "gurr";
+//    }
+}
